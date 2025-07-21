c3167e64a06122edfd8b97743a8f626538e366ce
wittyvishnu <vishnuwitty@gmail.com>
2025-07-21 10:42:23 +0530

    updated request limit

--- middlewares/rateLimiterMiddleware.js
+++ middlewares/rateLimiterMiddleware.js
@@ -1,9 +1,8 @@
-// middleware/rateLimiterMiddleware.js
 import rateLimit from 'express-rate-limit';
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per window
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 1000, // Limit each IP to 100 requests per window
   message: { message: 'Too many requests, please try again later' },
   standardHeaders: true,
   legacyHeaders: false,
